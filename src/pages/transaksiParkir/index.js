1afa7b5c3bf09665ac0b5fa35dcea338fc57d2b1
ichlasul0899 <ichlasul0899@gmail.com>
2020-12-19 13:58:36 +0700

    feat: finish

--- src/pages/transaksiParkir/index.js
+++ src/pages/transaksiParkir/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { TabContent, TabPane, Nav, NavItem, NavLink } from 'reactstrap';
 import { Container, Form, Button, FormControl, Alert } from 'react-bootstrap';
 import classnames from 'classnames';
-import ReactPaginate from 'react-paginate';
+// import ReactPaginate from 'react-paginate';
 import { Link } from 'react-router-dom';
 import { TableTransaksi, Loading } from '../../components';
 import { transaksiParkirService, memberService } from '../../services';
@@ -129,10 +129,11 @@ const TransaksiParkir = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState();
   const [activeTab, setActiveTab] = useState('1');
+
   const [totalData, setTotalData] = useState('');
   const [offset, setOffset] = useState(0);
   const [limit, setLimit] = useState('');
-  const [pageCount, setPageCount] = useState();
+  // const [pageCount, setPageCount] = useState();
 
   const toggle = (tab) => {
     if (activeTab !== tab) setActiveTab(tab);
@@ -140,84 +141,87 @@ const TransaksiParkir = () => {
 
   const fetchDataTransaksiAll = () => {
     // console.log('Hai iklas All');
-    setDataTransaksi([]);
-    setError(false);
-    setPageCount(0);
+    // setDataTransaksi([]);
+    // setError(false);
+    // setPageCount(0);
     setLoading(true);
-    // transaksiParkirService
-    //   .getTransaksiParkir(offset, limit)
-    //   .then((res) => {
-    //     setDataTransaksi(res.data);
-    //     setTotalData(res.total);
-    //     setPageCount(res.total / offset);
-    //   })
-    //   .catch((err) => {
-    //     setError(err);
-    //   })
-    //   .finally(() => {
-    //     setLoading(false);
-    //   });
+    transaksiParkirService
+      .getTransaksiParkir(offset, limit)
+      .then((res) => {
+        setDataTransaksi(res.data);
+        setTotalData(res.total);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
   const fetchDataTransaksiMasuk = () => {
-    // console.log('Hai iklas masuk');
     setLoading(true);
-    // transaksiParkirService
-    //   .getTransaksiParkir(offset, limit, 'ParkirMasuk')
-    //   .then((res) => {
-    //     setDataTransaksi(res.data);
-    //     setTotalData(res.total);
-    //     setPageCount(res.total / offset);
-    //   })
-    //   .catch((err) => {
-    //     setError(err);
-    //   })
-    //   .finally(() => {
-    //     setLoading(false);
-    //   });
+    transaksiParkirService
+      .getTransaksiParkir(offset, limit, 'ParkirMasuk')
+      .then((res) => {
+        setDataTransaksi(res.data);
+        setTotalData(res.total);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
   const fetchDataTransaksiKeluar = () => {
-    // console.log('Hai iklas out');
     setLoading(true);
-    // transaksiParkirService
-    //   .getTransaksiParkir(offset, limit, 'ParkirKeluar')
-    //   .then((res) => {
-    //     setDataTransaksi(res.data);
-    //     setTotalData(res.total);
-    //     setPageCount(res.total / offset);
-    //   })
-    //   .catch((err) => {
-    //     setError(err);
-    //   })
-    //   .finally(() => {
-    //     setLoading(false);
-    //   });
+    transaksiParkirService
+      .getTransaksiParkir(offset, limit, 'ParkirKeluar')
+      .then((res) => {
+        setDataTransaksi(res.data);
+        setTotalData(res.total);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
   useEffect(() => {
-    fetchDataTransaksiAll();
-  }, []);
+    setLoading(true);
+    transaksiParkirService
+      .getTransaksiParkir(offset, limit)
+      .then((res) => {
+        setDataTransaksi(res.data);
+        setTotalData(res.total);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [dataTransaksi, limit, offset]);
 
-  const handlePageClick = (e) => {
-    const selectedPage = e.selected;
-    setOffset(selectedPage * limit);
-    if (dataTransaksi[0].jenis === 'ParkirMasuk') {
-      fetchDataTransaksiMasuk(offset, limit);
-    } else if (dataTransaksi[0].jenis === 'ParkirKeluar') {
-      fetchDataTransaksiKeluar(offset, limit);
-    } else {
-      fetchDataTransaksiAll(offset, limit);
-    }
-  };
+  // const handlePageClick = (e) => {
+  //   const selectedPage = e.selected;
+  //   setOffset(selectedPage * limit);
+  //   if (dataTransaksi[0].jenis === 'ParkirMasuk') {
+  //     fetchDataTransaksiMasuk(offset, limit);
+  //   } else if (dataTransaksi[0].jenis === 'ParkirKeluar') {
+  //     fetchDataTransaksiKeluar(offset, limit);
+  //   } else {
+  //     fetchDataTransaksiAll(offset, limit);
+  //   }
+  // };
 
   return (
     <div>
-      <Container style={{ marginTop: '20px' }}>
-        <div style={{ margin: '20px', jsutifyContent: 'center' }}>
-          {error && <p>{error}</p>}
-          {loading && <Loading />}
-        </div>
+      <Container style={{ marginTop: '20px', padding: '20px' }}>
         <JudulTransaksi totalData={totalData} />
         <Search />
         <div>
@@ -227,7 +231,6 @@ const TransaksiParkir = () => {
                 style={{ color: 'black' }}
                 className={classnames({ active: activeTab === '1' })}
                 onClick={() => {
-                  setTotalData(0);
                   setOffset(0);
                   setLimit(10);
                   toggle('1');
@@ -242,7 +245,6 @@ const TransaksiParkir = () => {
                 style={{ color: 'black' }}
                 className={classnames({ active: activeTab === '2' })}
                 onClick={() => {
-                  setTotalData(0);
                   setOffset(0);
                   setLimit(10);
                   toggle('2');
@@ -256,7 +258,6 @@ const TransaksiParkir = () => {
               <NavLink
                 className={classnames({ active: activeTab === '3' })}
                 onClick={() => {
-                  setTotalData(0);
                   setOffset(0);
                   setLimit(10);
                   toggle('3');
@@ -267,19 +268,26 @@ const TransaksiParkir = () => {
               </NavLink>
             </NavItem>
           </Nav>
-          <TabContent activeTab={activeTab}>
-            <TabPane tabId="1">
-              {dataTransaksi && <TableTransaksi data={dataTransaksi} />}
-            </TabPane>
-            <TabPane tabId="2">
-              {dataTransaksi && <TableTransaksi data={dataTransaksi} />}
-            </TabPane>
-            <TabPane tabId="3">
-              {dataTransaksi && <TableTransaksi data={dataTransaksi} />}
-            </TabPane>
-          </TabContent>
+          {loading ? (
+            <div style={{ margin: '40px', jsutifyContent: 'center' }}>
+              {error && <Alert variant="danger">{error}</Alert>}
+              {loading && <Loading />}
+            </div>
+          ) : (
+            <TabContent activeTab={activeTab}>
+              <TabPane tabId="1">
+                {dataTransaksi && <TableTransaksi data={dataTransaksi} />}
+              </TabPane>
+              <TabPane tabId="2">
+                {dataTransaksi && <TableTransaksi data={dataTransaksi} />}
+              </TabPane>
+              <TabPane tabId="3">
+                {dataTransaksi && <TableTransaksi data={dataTransaksi} />}
+              </TabPane>
+            </TabContent>
+          )}
         </div>
-        <ReactPaginate
+        {/* <ReactPaginate
           previousLabel="<<<"
           nextLabel=">>>"
           breakLabel="..."
@@ -291,7 +299,7 @@ const TransaksiParkir = () => {
           containerClassName="pagination"
           subContainerClassName="pages pagination"
           activeClassName="active"
-        />
+        /> */}
       </Container>
     </div>
   );
